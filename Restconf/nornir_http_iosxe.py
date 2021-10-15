e5ac8dec97a0e5d29d1ea609a34ace4b48796144
orestis46 <orestis46@gmail.com>
2021-10-15 16:06:34 +0300

    testing filter models (config/nonconfig)

--- Restconf/nornir_http_iosxe.py
+++ Restconf/nornir_http_iosxe.py
@@ -1,3 +1,4 @@
+# Modules
 from nornir import InitNornir
 # from nornir_http.tasks import http_method
 from nornir.core.task import Task, Result
@@ -14,8 +15,10 @@ def get_restconf(task):
     requests.packages.urllib3.disable_warnings()
     headers = {"Accept": "application/yang-data+json"}
     # USE filter ?content=config or ?content=nonconfig for configuration or state/operational data only respectively
-    url = f"https://{task.host.hostname}:443/restconf/data/openconfig-interfaces:interfaces?content=nonconfig"
+    # NX-OS does not respond to ?content=nonconfig (no <model>-oper found in yang models)
+    #url = f"https://{task.host.hostname}:443/restconf/data/openconfig-interfaces:interfaces?content=nonconfig"
     #url = f"https://{task.host.hostname}:443/restconf/data/native"
+    url = f"https://{task.host.hostname}:443/restconf/data/openconfig-interfaces:interfaces?content=config"
 
     result = requests.get(
         url=url,
@@ -23,9 +26,13 @@ def get_restconf(task):
         auth=(f"{task.host.username}",
               f"{task.host.password}"),
         verify=False)
+    # IOS-XE returns content that will be formated in <dict> while NX-OS response will be in <str>
+    if(task.host.platform == "ios"):
+        result = json.loads(result.content.decode("utf-8"))
+    else:
+        result = result.text
     #task.host["facts"] = result.text
-    # result = json.loads(result.content.decode("utf-8"))
-    return Result(host=task.host, result=result.content)
+    return Result(host=task.host, result=result)
 
 
 nr = InitNornir(config_file="config.yaml")
